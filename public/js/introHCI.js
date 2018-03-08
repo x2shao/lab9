32dd5ea9e9e1ba018b012bb59089ade8837f6ede
Xinyu Shao <x2shao@ucsd.edu>
2018-03-07 20:24:29 -0800

    Commit message

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -7,19 +7,21 @@ $(document).ready(function() {
 
 // Function that is called when the document is ready.
 function initializePage() {
-    $.get('/lab/today', onServerResponse);
+    $.get('lab/today', onServerResponse);
+
+
 }
 
 // Function that is called when the server responds to the AJAX call
 function onServerResponse(data){
     var labElementId = getLabElementId(data.id);
     var labElement = $(labElementId);
-    labElement.css({'background': 'yellow'});
+    $(labElement).css({'background': 'yellow'});
 }
 
 
 // Function to format the CSS selector
 function getLabElementId(id){
-    var labElementId = '#lab_' + id;
+    var labElementId = '#lab-' + id;
     return labElementId;
 }
